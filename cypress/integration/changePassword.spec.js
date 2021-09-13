a109c32ab63c88cc726a9dcc1e9a44e24d6d941d
danilotodorovic6 <danilo.todorovic@vivifyideas.com>
2021-09-13 15:17:23 +0200

    Password updated with cy.writeFile

--- cypress/integration/changePassword.spec.js
+++ cypress/integration/changePassword.spec.js
@@ -3,9 +3,9 @@
 import { login } from "../page_object/login.js";
 import { account } from "../page_object/account.js";
 import data from "../fixtures/data.json";
-let faker = require("faker");
+import updatedPassword from "../fixtures/updatedPassword.json"
 
-let pass = "password123";
+let pass = "password124";
 
 Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from
@@ -16,19 +16,18 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 describe("Login", () => {
     beforeEach(() => {
         cy.visit("/login");
-        if(account.updatedPassword === false){
-            return login.loginClick(data.user.email, data.user.password);
+        if(updatedPassword.updatedPassword){
+            return login.loginClick(data.user.email, updatedPassword.updatedPassword);
         }
-        login.loginClick(data.user.email, account.updatedPassword);
         cy.url().should("contain", "my-organizations");
     })
     it("Account settings", () => {
+        cy.intercept("POST", "/api/v2/update-password").as("updatedPassword");
         account.changeAccountSettings();
-        if(account.updatedPassword === false){
-            return account.changePassword(data.user.password, pass);
-        }
-        account.changePassword(account.updatedPassword, pass);
-        cy.log(account.updatedPassword)
+        account.changePassword(updatedPassword.updatedPassword, pass);
+        cy.wait("@updatedPassword").then((intercept) => {
+            console.log(intercept);
+        })
     })
 
 })
